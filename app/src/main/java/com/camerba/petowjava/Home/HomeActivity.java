6fe07a616afc54597356eda6b37b52218c86086c
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-10 15:50:03 +0300

    initial commit

--- app/src/main/java/com/camerba/petowjava/Home/HomeActivity.java
+++ app/src/main/java/com/camerba/petowjava/Home/HomeActivity.java
@@ -1,18 +1,23 @@
 package com.camerba.petowjava.Home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.camerba.petowjava.LoginActivity;
 import com.camerba.petowjava.R;
 import com.camerba.petowjava.util.BottomNavigationViewHelper;
 import com.camerba.petowjava.util.UniversalImageLoader;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -25,6 +30,10 @@ public class HomeActivity extends AppCompatActivity {
     private final Context mContext = HomeActivity.this;
     //context calling
     private static final int ACTIVITY_NUM = 0;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,5 +96,54 @@ public class HomeActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
 
+    }
+
+    //firebase methods
+
+    private void checkCurrentUser(FirebaseUser user){
+        Log.d(TAG, "checkCurrentUser: checking if ser is logged in");
+
+        if (user == null){
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //checkCurrentUser
+                checkCurrentUser(user);
+
+                if (user != null) {
+                    //user is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed in" + user.getUid());
+                }else {
+                    //user is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed out");
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        checkCurrentUser(mAuth.getCurrentUser());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
